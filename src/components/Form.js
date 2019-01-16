1b856f7adb1cad5a5e11972090b24b2d093190ab
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-01-16 14:36:33 +0500

    Makes the components stateless

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import './../App.css';
 
-class Form extends Component {
-    render() {
-        return (
-            <div>
-                <p>form</p>
-                <form onSubmit={this.props.getWeather}>
-                    <input type="text" name={'city'} placeholder={'City Name'}/>
-                    <input type="text" name={'country'} placeholder={'Country Name'}/>
-                    <input type={'submit'} value={'Get Weather'}/>
-                </form>
+const Form=(props)=>(
+    <div>
+        <form onSubmit={props.getWeather}>
+            <input type="text" name={'city'} placeholder={'City Name'}/>
+            <input type="text" name={'country'} placeholder={'Country Name'}/>
+            <input type={'submit'} value={'Get Weather'}/>
+        </form>
 
-            </div>
-        );
-    }
-}
+    </div>
+)
 
 export default Form;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import './../App.css';
 
-class Weather extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.city && this.props.country && <p>Location :  {this.props.city},{this.props.country} </p>}
-                {this.props.temperature && <p>temperature:  {this.props.temperature} </p>}
-                {this.props.humidity && <p>humidity :  {this.props.humidity} </p>}
-                {this.props.description && <p>Description :  {this.props.description} </p>}
-                {this.props.error && <p>{this.props.error} </p>}
-            </div>
-        );
-    }
-}
+const Weather=(props)=> (
+    <div>
+        {props.city && props.country && <p>Location :  {props.city},{props.country} </p>}
+        {props.temperature && <p>temperature:  {props.temperature} </p>}
+        {props.humidity && <p>humidity :  {props.humidity} </p>}
+        {props.description && <p>Description :  {props.description} </p>}
+        {props.error && <p>{props.error} </p>}
+    </div>
+)
+
 
 export default Weather;
